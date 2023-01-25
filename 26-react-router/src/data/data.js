f7a6d622af7c64dffa90b7c01f2f6ffe937c33c2
Dmitry Morgunov <morgunovd0401@gmail.com>
2023-01-25 16:23:11 +0600

    React-router

---
+++ 26-react-router/src/data/data.js
@@ -0,0 +1,23 @@
+const courses = [
+  {
+    title: "Complete React Guide",
+    slug: "react",
+    id:257
+  },
+  {
+    title: "Complete JavaScript Guide",
+    slug: "javascript",
+    id:154
+  },
+  {
+    title: "Complete Python Guide",
+    slug: "python",
+    id:352
+  },
+  {
+    title: "Complete NodeJs Guide",
+    slug: "node-js",
+    id:123
+  }
+]
+export default courses
