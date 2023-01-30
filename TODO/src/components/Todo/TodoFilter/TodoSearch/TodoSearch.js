9854969edbe8dbb9cfc0943ac4e1da926625261e
Dmitry Morgunov <morgunovd0401@gmail.com>
2023-01-30 18:35:09 +0600

    Final version

--- TODO/src/components/Todo/TodoFilter/TodoSearch/TodoSearch.js
+++ TODO/src/components/Todo/TodoFilter/TodoSearch/TodoSearch.js
@@ -1,12 +1,20 @@
 import styles from "./TodoSearch.module.css";
 import Input from "../../../UI/Input";
 import Button from "../../../UI/Button/Button";
+import { useState } from "react";
 function TodoSearch({ setSearchText }) {
+  const [value,setValue] = useState()
+  const handleSearch = (e) => {
+    e.preventDefault()
+    setSearchText(value)
+    setValue('')
+  }
+  console.log(value)
   return (
-    <div className={styles.todoSearchContainer}>
-      <Input onChange={(e) => setSearchText(e.target.value)} placeholder="Поиск..." />
-      <Button>Найти</Button>
-    </div>
+    <form onSubmit={handleSearch} className={styles.todoSearchContainer}>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} placeholder="Поиск..." />
+      <Button  type="submit">Найти</Button>
+    </form>
   );
 }
 export default TodoSearch;
